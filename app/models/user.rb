f141dd78068834db2f65e9cf687e4e2381f4ff30
Onur Sahin <anionrush@gmail.com>
2016-02-29 00:48:09 -0800

    checkpoint 39

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,9 @@
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
-
- # #3
-   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
- # #4
+	has_many :posts
+  validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
   validates :password, length: { minimum: 6 }, allow_blank: true
- # #5
-   validates :email,
-             presence: true,
-             uniqueness: { case_sensitive: false },
-             length: { minimum: 3, maximum: 254 }
-
- # #6
-   has_secure_password
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 254 }
+	has_secure_password
 end
