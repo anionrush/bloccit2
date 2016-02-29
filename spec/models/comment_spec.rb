450cb5d0d7eadaa1eff3880a22e12d0309ac5ca1
Onur Sahin <anionrush@gmail.com>
2016-02-28 20:40:39 -0800

    checkpoint 34

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+     let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+   let(:comment) { Comment.create!(body: 'Comment Body', post: post) }
+ 
+   describe "attributes" do
+     it "responds to body" do
+       expect(comment).to respond_to(:body)
+     end
+   end
+end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
+ # #4
+   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
+ 
+   it { is_expected.to belong_to(:topic) }
+
+   it { is_expected.to validate_presence_of(:title) }
+   it { is_expected.to validate_presence_of(:body) }
+   it { is_expected.to validate_presence_of(:topic) }
+ # 2
+   it { is_expected.to ensure_length_of(:title).is_at_least(5) }
+   it { is_expected.to ensure_length_of(:body).is_at_least(20) }
+ 
+   describe "attributes" do
+ # #2
+     it "responds to title" do
+       expect(post).to respond_to(:title)
+     end
+ # #3
+     it "responds to body" do
+       expect(post).to respond_to(:body)
+     end
+   end
+end
