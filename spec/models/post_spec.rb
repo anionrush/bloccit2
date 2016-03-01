9931c19103ae2f8be4b260d6d1b6bca53196ccd8
Conrad Taylor <conradwt@gmail.com>
2016-02-29 16:48:41 -0800

    Change ensure_length_of to validate_length_of per should warning.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Post, type: :model do
  # #4
    let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
    let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
- 
+
    it { is_expected.to belong_to(:topic) }
    it { is_expected.to belong_to(:user) }
 
@@ -14,9 +14,9 @@ RSpec.describe Post, type: :model do
    it { is_expected.to validate_presence_of(:topic) }
    it { is_expected.to validate_presence_of(:user) }
  # 2
-   it { is_expected.to ensure_length_of(:title).is_at_least(5) }
-   it { is_expected.to ensure_length_of(:body).is_at_least(20) }
- 
+   it { is_expected.to validate_length_of(:title).is_at_least(5) }
+   it { is_expected.to validate_length_of(:body).is_at_least(20) }
+
    describe "attributes" do
  # #2
      it "responds to title" do
