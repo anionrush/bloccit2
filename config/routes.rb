ce0aca7bf0cb6a1e6bfab124bb69b53b8243f689
Onur Sahin <anionrush@gmail.com>
2016-03-04 02:11:55 -0800

    Checkpoint-41

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   resources :topics do  
     resources :posts, except: [:index]
   end
+
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
+
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
   get 'about' => 'welcome#about'
