104219d37ec6d896f525da2113958bac8ffba158
Onur Sahin <anionrush@gmail.com>
2016-03-06 22:42:16 -0800

    assignment 43

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ActiveRecord::Base
 	has_many :votes, dependent: :destroy
 	has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
+  after_create :create_vote
 
 	default_scope { order('rank DESC') }
 	scope :order_by_title, -> {order('title DESC')}
@@ -33,4 +34,9 @@ class Post < ActiveRecord::Base
      new_rank = points + age_in_days
      update_attribute(:rank, new_rank)
    end
+
+  private 
+    def create_vote
+    user.votes.create(value: 1, post: self)
+  end
 end
