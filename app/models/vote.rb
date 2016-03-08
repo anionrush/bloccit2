9ea700e83299c3e8642ce5bc35232fc9641740e5
Conrad Taylor <conradwt@gmail.com>
2016-03-07 17:12:13 -0800

    Code cleanup.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,10 +2,10 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   
+  validates :value, inclusion: { in: [-1, 1], message: "%{value} is not a valid vote." }, presence: true
+  
   after_save :update_post
   
-  validates :value, inclusion: { in: [-1, 1], message: "%{value} is not a valid vote." }, presence: true
-
   private
  
   def update_post
