3f13342a074260717081d930dcc669d0551d5c74
Onur Sahin <anionrush@gmail.com>
2016-03-09 20:09:19 -0800

    Assignment-42 try 2

--- db/migrate/20160229003632_create_comments.rb
+++ db/migrate/20160229003632_create_comments.rb
@@ -2,9 +2,11 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :body
-      t.references :post, index: true, foreign_key: true
+      t.integer :commentable_id
+      t.string :commentable_type
 
       t.timestamps null: false
     end
+
   end
 end
