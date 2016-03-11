7311e459c0f641124ea10b1b25d879bcd48797ce
Conrad Taylor <conradwt@gmail.com>
2016-03-11 02:20:54 -0800

    Add user_id to the comments table.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,59 +1,74 @@
 require 'random_data'
-#Create Users
+
+# Create Users
 5.times do
-   User.create!(
-   name:     RandomData.random_name,
-   email:    RandomData.random_email,
-   password: RandomData.random_sentence
-   )
- end
- users = User.all
+  User.create!(
+  name:     RandomData.random_name,
+  email:    RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+
+users = User.all
 
 # Create Topics
 15.times do
- Topic.create!(
-   name:         RandomData.random_sentence,
-   description:  RandomData.random_paragraph
- )
+  Topic.create!(
+  name:         RandomData.random_sentence,
+  description:  RandomData.random_paragraph
+  )
 end
+
 topics = Topic.all
 
-#Create Posts
-50.times do 
-	Post.create!(
-    user:   users.sample,
-		topic:  topics.sample,
-		title:  RandomData.random_sentence,
-    body:   RandomData.random_paragraph )
-	end
- posts = Post.all
- 
- # Create Comments
- 100.times do
-   Comment.create!(
-     user: users.sample,
-     #post: posts.sample,
-     body: RandomData.random_paragraph
-   )
- end
+# Create Topic Comments
+
+topics.each do | topic |
+  topic.comments.create!( body: RandomData.random_paragraph, user: users.sample )
+end
+
+# Create Posts
+
+50.times do
+  Post.create!(
+  user:   users.sample,
+  topic:  topics.sample,
+  title:  RandomData.random_sentence,
+  body:   RandomData.random_paragraph )
+end
+
+posts = Post.all
+
+# Create Comments
+
+posts.each do | post |
+  post.comments.create!( body: RandomData.random_paragraph, user: users.sample )
+end
+
+# 100.times do
+#   Comment.create!(
+#   user: users.sample,
+#   body: RandomData.random_paragraph
+#   )
+# end
 
 # Create an admin
 admin = User.create!(
-   name:     'Admin User',
-   email:    'admin@example.com',
-   password: 'helloworld',
-   role:     'admin'
- )
- 
- # Create a member
- member = User.create!(
-   name:     'Member User',
-   email:    'member@example.com',
-   password: 'helloworld'
- )
- 
- puts "Seed finished"
- puts "#{User.count} users created"
- puts "#{Topic.count} topics created"
- puts "#{Post.count} posts created"
- puts "#{Comment.count} comments created"
+name:     'Admin User',
+email:    'admin@example.com',
+password: 'helloworld',
+role:     'admin'
+)
+
+# Create a member
+member = User.create!(
+name:     'Member User',
+email:    'member@example.com',
+password: 'helloworld'
+)
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Topic.count} topics created"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
